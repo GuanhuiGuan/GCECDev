e4e92f42e4efa2d18618869b3fb1ee6735fbb5b3
Guanhui Guan <guanhuiguan@Peter-Guans-Mac.local>
2019-11-17 20:55:08 -0800

    Modify version to 0.2.0

--- GCECDev/Constants/Constants.cs
+++ GCECDev/Constants/Constants.cs
@@ -6,7 +6,7 @@ namespace GCECDev.Constants
     public static class Constants
     {
 
-        public static string Version = "0.1.5";
+        public static string Version = "0.2.0";
 
         public static string Footer = string.Format("Copyright WGCF 2020 | Ver. {0}", Version);
 
